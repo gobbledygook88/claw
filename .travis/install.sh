c65328063ae8de9d8e46ead0f7d057d268b82d4c
Marcus Mo <marcusmo88@gmail.com>
2018-12-24 12:05:21 +0000

    Install under sudo

--- .travis/install.sh
+++ .travis/install.sh
@@ -4,8 +4,8 @@
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
     brew install shellcheck
 else
-    apt-get install shellcheck
+    sudo apt-get install shellcheck
 fi
 
 # Common dependencies
-pip install bashate
+sudo pip install bashate
