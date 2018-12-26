d2a465d38fd0e2785d729f6860d254d68c2e1c39
Marcus Mo <marcusmo88@gmail.com>
2018-12-26 23:36:42 +0000

    Place depth argument before name in find command
    Allows for find command to run on linux

--- tests/run_all.sh
+++ tests/run_all.sh
@@ -3,7 +3,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd "$DIR" || exit 1
 
-TEST_FILES="$(find . -name 'test_*.sh' -type f -depth 1 -exec basename {} \;)"
+TEST_FILES="$(find . -depth 1 -type f -name 'test_*.sh' -exec basename {} \;)"
 NUM_TEST_FILES="$(echo "$TEST_FILES"  | tr ' ' '\n' | wc -l | awk '{print $1}')"
 
 echo "Found $NUM_TEST_FILES test files."
