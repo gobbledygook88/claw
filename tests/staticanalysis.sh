1d6f11458067e0fed58015e9f537dddf60253611
Marcus Mo <marcusmo88@gmail.com>
2018-12-26 16:59:18 +0000

    Run shellcheck only on src shell scripts i.e. ignore test files

--- tests/staticanalysis.sh
+++ tests/staticanalysis.sh
@@ -4,11 +4,12 @@ set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-find "$DIR/.." \
+# Run shellcheck on only src directory
+find "$DIR/../src" \
     -name "*.sh" \
-    -not -name "sharness.sh" \
-    -not -name "aggregate-results.sh" \
     -exec shellcheck -x {} +
+
+# Run bashate on all shell scripts (including tests)
 find "$DIR/.." \
     -name "*.sh" \
     -not -name "sharness.sh" \
