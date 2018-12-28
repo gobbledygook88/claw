9974dff1e9744af58c9f751e7b6aac6b8cfba2ec
Marcus Mo <marcusmo88@gmail.com>
2018-12-28 21:41:03 +0000

    Install prerequisites

--- .travis/install.sh
+++ .travis/install.sh
@@ -2,7 +2,7 @@
 
 # OS specific dependencies
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
-    brew install shellcheck
+    brew install bash bash-completion git gnu-getopt shellcheck tree
 else
     sudo apt-add-repository "deb http://archive.ubuntu.com/ubuntu trusty-backports main restricted universe multiverse"
     sudo apt-get -qq update
